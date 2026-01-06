7e4a70da94511d3268b0edf25e74f956998cbf82
Aananth C N <c.n.aananth@gmail.com>
2026-01-06 22:25:05 +0530

    2nd commit - after successful build and run
    This commit only has a basic screen (SDL) with a label printed.

--- README.md
+++ README.md
@@ -8,6 +8,34 @@
 Also do the following once:
  * `conan remote update conancenter --url https://center2.conan.io`
 
+### MinGW64
+
+`vi $(conan profile path default)`
+
+Then add the following inside it
+```
+[settings]
+os=Windows
+arch=x86_64
+compiler=gcc
+compiler.version=15
+compiler.libcxx=libstdc++11
+compiler.cppstd=gnu17
+build_type=Release
+
+[conf]
+# 1. Force the exact MinGW executables (Prevents picking up MSVC)
+tools.build:compiler_executables={'c': 'C:/msys64/mingw64/bin/gcc.exe', 'cpp': 'C:/msys64/mingw64/bin/g++.exe'}
+
+# 2. Relax GCC 15 strictness (Fixes the _stati64 / incompatible types errors)
+tools.build:cflags=["-Wno-error=incompatible-pointer-types", "-fpermissive"]
+tools.build:cxxflags=["-fpermissive"]
+
+[buildenv]
+# 3. Ensure MinGW is at the front of the PATH during builds
+PATH+=(path)C:/msys64/mingw64/bin
+```
+
 
 # Build Commands
 ## Install dependencies:
--- conanfile.py
+++ conanfile.py
@@ -1,21 +1,18 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
-import os
+from conan.tools.cmake import CMake, cmake_layout
 
 class CarIQUIRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
-        self.requires("lvgl/9.1.0")
+        # SDL is still handled by Conan
         self.requires("sdl/2.30.1")
 
     def layout(self):
         cmake_layout(self)
 
-    def generate(self):
-        # self.recipe_folder is the absolute path to your App's root
-        header_path = os.path.join(self.recipe_folder, "src", "lv_conf.h")
-        # Ensure we use forward slashes for the conf string to avoid escape char issues
-        header_path = header_path.replace("\\", "/")
-        self.conf_info.define("user.lvgl:config_path", header_path)
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
